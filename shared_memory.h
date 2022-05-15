697e99434237fe2d341b2b217755cb1f56cda237
stefan891 <razvanstefan.manole@studenti.univr.it>
2022-05-15 17:51:59 +0200

    shared memory files

--- shared_memory.h
+++ shared_memory.h
@@ -3,3 +3,18 @@
 ///         specifiche per la gestione della MEMORIA CONDIVISA.
 
 #pragma once
+
+#define _SHARED_MEMORY_HH
+
+#include <stdlib.h>
+
+int alloc_shared_memory(key_t shmKey, size_t size);
+
+void *get_shared_memory(int shmid, int shmflg);
+
+void free_shared_memory(void *ptr_sh);
+
+void remove_shared_memory(int shmid);
+
+
+
